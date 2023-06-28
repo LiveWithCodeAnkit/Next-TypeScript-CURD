7071498b6c3ddc61be0adc285d2fee6fcf464a9d
LiveWithCodeAnkit <ankityadav119live@gmail.com>
2023-06-28 23:34:57 +0530

    updated code

--- src/Component/Profile/ViewInfo.tsx
+++ src/Component/Profile/ViewInfo.tsx
@@ -7,30 +7,35 @@ interface dataItem {
   name: string;
   gender: string;
   dob: string;
-  hobby: any[];
+  hobby: string[];
+  hq: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+  profileImg: string;
 }
 
 const ViewInfo = () => {
-  const storedData = useLogged();
-  const array = [storedData];
+  const storedData: any = useLogged();
+
   return (
     <>
-      <>
-        <div>
-          <h1>Welcome to</h1>
-          <ul>
-            {array.map((dataItem: dataItem,index) => (
-              <li key={index}>
-                <p>Name: {dataItem.name}</p>
-                <p>Gender: {dataItem.gender}</p>
-                <p>Phone: {dataItem.phone}</p>
-                <p>DOB: {dataItem.dob}</p>
-                <p>Hobby: {dataItem.hobby}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </>
+      <div>
+        <h1>Welcome to</h1>
+        <ul>
+          <li>
+            <p>Name: {storedData.name}</p>
+            <p>Gender: {storedData.gender}</p>
+            <p>Phone: {storedData.phone}</p>
+            <p>DOB: {storedData.dob}</p>
+            {storedData.hobby && Array.isArray(storedData.hobby) && (
+              <p>Hobby: {storedData.hobby.join(", ")}</p>
+            )}
+            <p>Highest Qualification: {storedData.hq}</p>
+            <p>Email: {storedData.email}</p>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
